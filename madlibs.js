495a1b65f5d45ae732c2b47c88fc30038aa64079
nicole <nrodgers@cshnyc.org>
2018-11-07 14:46:10 -0500

    initial commit

---
+++ madlibs.js
@@ -0,0 +1,26 @@
+var adj1=prompt("Insert an adjective");
+var age=prompt('Insert a number');
+var animal=prompt('Insert an animal');
+var verb1=prompt('Insert a verb');
+var bodyp=prompt('Insert a body part');
+var weather=prompt ('Type of weather');
+var place=prompt('Insert a place');
+var number=prompt ('Insert a number');
+var time= number -2
+var adj2=prompt('Insert an adjective');
+var noun=prompt ('Insert a noun');
+var adj3=prompt('Insert an adjective');
+var flavor=prompt ('Insert a flavor');
+
+
+
+document.write ("One "+ adj1 +" day a man was walking down the street\
+.He was rushing to his daughter's "+ age + " th birthday party,\
+ when all the sudden a/an " + animal+ "\
+came out of nowhere and "+ verb1+ "ed on his "+bodyp+ "\
+ The angry man yelled at the " +animal+ "\
+ .Then it started to "+weather+".After it stopped he realized it was already "
++ number+" o'clock, and the party started at "+time+ " o'clock! \
+He was so late! He finally got to the party \
+and told his daughter how "+adj2+" he was.\
+He gave her a/an "+noun+" and a " + adj3 +" "+ flavor+ " cake.");
